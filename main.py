4178daf4af53038df3ca50688416b7d29e8f3376
DESKTOP-GGDLLLK\Javo <javo.iglesias@gmail.com>
2023-05-17 14:14:27 -0300

    Main added an infinite loop to deal dev cards

--- main.py
+++ main.py
@@ -3,13 +3,30 @@ import devLogic
 
 def main():
     board = boardLogic.create_board()
-
     boardLogic.print_board(board)
 
-    deck = devLogic.Deck()
-    deck.initialize_deck()
-    deck.shuffle()
-    print(deck.draw_card().type)
+    deck = devLogic.Deck()  # Note the parentheses here
+    deck.shuffle()  # Shuffle the deck before starting the loop
+
+    while True:
+        print("\nActions:")
+        print("1. Draw card")
+        print("2. Check remaining cards")
+        print("3. Exit")
+        user_input = input("Choose an action: ")
+
+        if user_input == "1":
+            card = deck.draw_card()
+            if card is not None:
+                print(f"You drew a {card.type} card.")
+            else:
+                print("No more cards in the deck.")
+        elif user_input == "2":
+            print(f"There are {deck.cards_remaining()} cards remaining in the deck.")
+        elif user_input == "3":
+            break  # Exit the loop
+        else:
+            print("Invalid input. Please enter a number between 1 and 3.")
 
 if __name__ == "__main__":
     main()
