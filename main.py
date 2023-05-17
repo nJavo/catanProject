29b6ec2beac1d803fc842f98629bbdffe542af69
DESKTOP-GGDLLLK\Javo <javo.iglesias@gmail.com>
2023-05-17 13:48:36 -0300

    test

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import boardLogic
+import devLogic
 
 def main():
     board = boardLogic.create_board()
 
     boardLogic.print_board(board)
 
+    deck = devLogic.Deck
+    deck.initialize_deck(deck)
+    print(deck.draw_card(deck))
 
 if __name__ == "__main__":
     main()
